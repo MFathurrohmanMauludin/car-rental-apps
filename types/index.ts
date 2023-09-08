78f155d1cf4c9bbea52bee6d84fbfe4618b6c0c7
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-08 19:09:42 +0700

    update

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,5 @@ export interface CustomButtonProps {
     title: string;
     containerStyles?: string; // TODO ?: opsional /nilainya itu bisa ditentukan atau tidak tentukan
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
 }
