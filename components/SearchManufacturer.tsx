868213c643524c1ed0c7a8fbd51458082d2317cd
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-09 21:18:26 +0700

    update

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,8 +1,41 @@
-import { SearchManufacturerProps } from '@/types'
+"use client";
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { Combobox, Transition } from '@headlessui/react';
+import { SearchManufacturerProps } from '@/types';
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+    const [query, setQuery] = useState('');
+
     return (
-        <div>SearchManufacturer</div>
+        <div className="search-manufacturer">
+            <Combobox>
+                <div className="relative w-full">
+                    <Combobox.Button className="absolute top-[14px]">
+                        <Image
+                            src="/car-logo.svg"
+                            width={20}
+                            height={20}
+                            className="ml-4"
+                            alt="Car Logo"
+                        />
+                    </Combobox.Button>
+
+                    <Combobox.Input
+                        className="search-manufacturer__input"
+                        placeholder="Volkswagen"
+                        displayValue={(manufacturer: string) => manufacturer}
+                        onChange={(e) => setQuery(e.target.value)}
+                    >
+
+                        <Transition>
+
+                        </Transition>
+                    </Combobox.Input>
+                </div>
+            </Combobox>
+        </div>
     )
 }
 
