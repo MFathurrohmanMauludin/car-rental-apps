94a57c7160777d5c5a7ccb13ac4517c76161bae2
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-10 05:28:08 +0700

    update

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,10 +3,18 @@
 import { useState, Fragment } from 'react';
 import Image from 'next/image';
 import { Combobox, Transition } from '@headlessui/react';
+
+import { manufacturers } from '@/constants';
 import { SearchManufacturerProps } from '@/types';
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
     const [query, setQuery] = useState('');
+    // TODO: menampilkan semua merek jika input kosong, tampilkan merek secara spesifik 
+    const filteredManufacturers = query === "" ? manufacturers : manufacturers.filter((item) => (
+        item.toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+    ))
 
     return (
         <div className="search-manufacturer">
@@ -34,12 +42,32 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                         leave="transition ease-in duration-100"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
+                        afterLeave={() => setQuery('')}
                     >
-
+                        <Combobox.Options>
+                            {filteredManufacturers.length === 0 && query !== "" ? (
+                                <Combobox.Option
+                                    value={query}
+                                    className="search-manufacturer__option"
+                                >
+                                    Create "{query}"
+                                </Combobox.Option>
+                            ) : (
+                                filteredManufacturers.map((item) => (
+                                    <Combobox.Option
+                                        key={item}
+                                        className={({ active }) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
+                                        value={item}
+                                    >
+                                        {item}
+                                    </Combobox.Option>
+                                )
+                                ))}
+                        </Combobox.Options>
                     </Transition>
                 </div>
             </Combobox >
-        </div>
+        </div >
     )
 }
 
