86f72cd5d01a40e7314d93f5a4dcbdbcfa5aed4f
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-09 18:26:34 +0700

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,6 +6,7 @@ import { footerLinks } from '@/constants';
 const Footer = () => {
     return (
         <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+            //TODO: brand & link bussiness
             <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
                 <div className="flex flex-col justify-start items-start gap-6">
                     <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
@@ -32,6 +33,7 @@ const Footer = () => {
                 </div>
             </div>
 
+            //TODO: copyrights
             <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
                 <p>@2023 CarHub. All Rights Reserved </p>
                 <div className="footer__copyrights-link">
