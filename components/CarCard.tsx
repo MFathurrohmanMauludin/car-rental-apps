8f8cdc59432adf8a8b01712eacb40ee873df57e7
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-10 08:48:22 +0700

    update

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,9 +3,30 @@
 import { useState } from 'react';
 import Image from 'next/image';
 
-const CarCard = ({ car }) => {
+import { CarProps } from '@/types';
+import CustomButton from './CustomButton';
+
+interface CarCardProps {
+    car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+    const { city_mpg, year, make, model, transmission, drive } = car;
+
     return (
-        <div>CarCard</div>
+        <div className="car-card group">
+            <div className="car-card__content">
+                <h2 className="car-card__content-title">
+                    {make} {model}
+                </h2>
+            </div>
+
+            <p>
+                <span>
+                    Car Rent...
+                </span>
+            </p>
+        </div>
     )
 }
 
