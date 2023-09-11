92ab5f18f92f66a168346f69878fe0ca9c83b2d1
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-11 15:43:36 +0700

    update

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+"use client";
 
-const CustomFilter = () => {
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Listbox, Transition } from "@headlessui/react";
+import { CustomeFilterProps } from "@/types";
+
+const CustomFilter = ({ title, options }: CustomeFilterProps) => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
-        <div>CustomFilter</div>
+        <div className="w-fit">
+            <Listbox>
+                <div className="relative w-fit z-10">
+                    <Listbox.Button className="custom-bilter__btn">
+                        <span>Filter</span>
+                    </Listbox.Button>
+                </div>
+            </Listbox>
+        </div>
     )
 }
 
